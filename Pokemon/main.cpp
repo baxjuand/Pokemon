5c3e9d18d2da3bad9d92f4e9a21b3a8c7f78b90d
baxjuand <baxjuand@gmail.com>
2025-03-26 20:57:03 -0500

    My main

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+using namespace std;
 
-int main() {
+int main() 
+{
+    string player_name;
+    
+    cout << "Enter your name: " << endl;
+
+    cin >> player_name;
+
+    cout << "Great Start " << player_name << ", looks like you have understood the main() function properly" << endl;
 
     return 0;
 }
