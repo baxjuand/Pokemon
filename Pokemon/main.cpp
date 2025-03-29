f150ddf08b730b1c50f59bc094556b9cbfd03e18
baxjuand <baxjuand@gmail.com>
2025-03-29 09:11:43 -0500

    Created Enum for pokemon choices. Added switch statements for decision.

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,12 +2,21 @@
 #include <string>
 using namespace std;
 
+enum PokemonChoice
+{
+    Bulbasaur,
+    Charmander,
+    Squirtle,
+    Pikachu,
+    InvalidChoice
+};
+
 int main ()
 {
 
     string playerName;
     int pokemonChoice;
-    string chosenPokemon;
+    PokemonChoice chosenPokemon = InvalidChoice;
 
     cout << "Professor Oak:" << endl;
     cout << "Ah, Trainer!" << endl;
@@ -28,26 +37,53 @@ int main ()
     switch(pokemonChoice)
     {
         case 1:
-        chosenPokemon = "Bulbasaur";
-        cout << "You chose Bulbasaur! A wise choice." << endl;
+        chosenPokemon = Bulbasaur;
         break;
 
         case 2:
-        chosenPokemon = "Charmander";
-        cout << "You chose Charmander! A fiery choice." << endl;
+        chosenPokemon = Charmander;
         break;
 
         case 3:
-        chosenPokemon = "Squirtle";
-        cout << "You chose Squirtle! A cool choice." << endl;
+        chosenPokemon = Squirtle;
+        break;
+
+        case 7:
+        chosenPokemon = Pikachu;
         break;
 
         default:
-        chosenPokemon = "Pikachu";
-        cout << "You saw that other pokéball, didn't you...?\nHere, you can have Pikachu!" << endl;
+        chosenPokemon = InvalidChoice;
     }
 
-    cout << "Ah, an excellent choice!" << endl;
+    switch (chosenPokemon)
+    {
+        case Bulbasaur:
+        cout << "You chose Bulbasaur! A wise choice." << endl;
+        break;
+
+        case Charmander:
+        cout << "You chose Charmander! A fiery choice." << endl;
+        break;
+
+        case Squirtle:
+        cout << "You chose Squirtle! A cool choice." << endl;
+        break;
+
+        case Pikachu:
+        cout << "You saw that other pokéball, didn't you...?" << endl;
+        cout << "You chose Pikachu! An electrifying turn of events." << endl;
+        break;
+
+        default:
+        cout << "Hmm, that doesn't seem right. Let me choose for you...\n";
+        chosenPokemon = Charmander;
+        cout << "Let's go with Charmander, the fiery dragon in the making!\n";
+        break;
+    }
+
+    cout << "Ah, an excellent choice! " << (chosenPokemon == Bulbasaur ? "Bulbasaur" : chosenPokemon == Charmander ? "Charmander" : chosenPokemon == Squirtle ? "Squirtle" : "Pikachu")
+         << " and you, will make a great team!" << endl;
     cout << "But beware, " << playerName << "," << endl;
     cout << "this is only the beginning." << endl;
     cout << "Your journey is about to unfold." << endl;
